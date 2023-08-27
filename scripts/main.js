81cc1d2a4ba6ea743e1b3a9cbbf37e5386a3fb77
miguel <91511269+ghmacg@users.noreply.github.com>
2023-08-27 14:58:36 -0400

    Add function to call one of the operator functions depending on whats input

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,10 @@ const add = (num1, num2) => num1 + num2;
 const substract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
+
+function operate (operator, num1, num2) {
+    operator == '+' ? add(num1, num2) :
+        operator == '-' ? substract(num1, num2) :
+            operator == 'x' ? multiply(num1, num2) :
+                operator == '÷' ? divide(num1, num2) : operator;
+}
