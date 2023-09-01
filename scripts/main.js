4ba95ae16f8a679e7860c2339dd43388eab6815c
miguel <91511269+ghmacg@users.noreply.github.com>
2023-08-31 20:21:49 -0400

    Create basic operation functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
 let firstNum = '';
 let secondNum = '';
 let currentOperator = null;
+
+const add = (x, y) => x + y;
+const substract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => y == 0 ? 'ERROR: Cannot divide by 0' : x / y;
