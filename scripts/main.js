21c04419925ac7875969888a79279af086dd3aeb
miguel <91511269+ghmacg@users.noreply.github.com>
2023-08-31 20:20:53 -0400

    Start from zero again declaring essential variables

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,3 @@
-let firstNum;
-let secondNum;
-let operator;
-let displayValue = 0;
-
-const add = (x, y) => x + y;
-const substract = (x, y) => x - y;
-const multiply = (x, y) => x * y;
-const divide = (x, y) => x / y;
-
-function operate (operator, x, y) {
-    operator == '+' ? add(x, y) :
-        operator == '-' ? substract(x, y) :
-            operator == 'x' ? multiply(x, y) :
-                operator == '÷' ? divide(x, y) : operator;
-};
+let firstNum = '';
+let secondNum = '';
+let currentOperator = null;
