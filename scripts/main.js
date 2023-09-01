996504ecf4335d61cd7a6d586fa24e8a24be9133
miguel <91511269+ghmacg@users.noreply.github.com>
2023-08-31 20:22:42 -0400

    Create function to operate depending on operator

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,12 @@ const add = (x, y) => x + y;
 const substract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => y == 0 ? 'ERROR: Cannot divide by 0' : x / y;
+
+function operate (operator, x, y) {
+    operator === '+' ? firstNum = add(x, y) :
+        operator === '-' ? firstNum = substract(x, y):
+            operator === 'x' ? firstNum = multiply(x, y):
+                operator === '÷' ? firstNum = divide(x, y): operator;
+    
+    secondNum = '';
+};
